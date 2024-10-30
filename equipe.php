bbee8724c7bc6e4df30715e8c4a2930aac034ebc
tiagoprogwebt2 <tiago.progweb2024t2@gmail.com>
2024-10-30 14:21:03 -0300

    Create equipe.php

---
+++ equipe.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Equipe - Site</title>
+</head>
+<body>
+    <header>
+        <h1>Site Exemplo</h1>
+        <nav>
+            <a href="">Home</a>
+            <a href="">Equipe</a>
+            <a href="">Serviços</a>
+        </nav>
+    </header>
+    
+    <main>
+        <article>
+            <h2>Nossa equipe</h2>
+            <ul>
+                <li>Tony Stark</li>
+                <li>Bruce Wayne</li>
+                <li>Racha Cuca</li>
+            </ul>
+        </article>
+    </main>
+    
+    <footer>
+        <h2>Site criado como exemplo</h2>
+    </footer>
+</body>
+</html>

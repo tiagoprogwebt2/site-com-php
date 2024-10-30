f9a8a05769d93b8acbe13dbc6b6ae1328ff387a2
tiagoprogwebt2 <tiago.progweb2024t2@gmail.com>
2024-10-30 14:23:47 -0300

    Create servicos.php

---
+++ servicos.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Serviços - Site</title>
+</head>
+<body>
+    <header>
+        <h1>Site Exemplo</h1>
+        <nav>
+            <a href="">Home</a>
+            <a href="">Equipe</a>
+            <a href="">Serviços</a>
+        </nav>
+    </header>
+    
+    <main>
+        <article>
+            <h2>O que fazemos?</h2>
+            <ol>
+                <li>Sites</li>
+                <li>Aplicativos</li>
+                <li>Jogos</li>
+            </ol>
+        </article>
+    </main>
+    
+    <footer>
+        <h2>Site criado como exemplo</h2>
+    </footer>
+</body>
+</html>

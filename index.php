b2cce232afecf729151182a4846a0cd675d6ccb4
tiagoprogwebt2 <tiago.progweb2024t2@gmail.com>
2024-10-30 14:16:50 -0300

    Create index.php

---
+++ index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Site</title>
+</head>
+<body>
+    <header>
+        <h1>Site Exemplo</h1>
+        <nav>
+            <a href="">Home</a>
+            <a href="">Equipe</a>
+            <a href="">Serviços</a>
+        </nav>
+    </header>
+    
+    <main>
+        <article>
+            <h2>Bem-vindo(a)!</h2>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel, quia?</p>
+        </article>
+    </main>
+    
+    <footer>
+        <h2>Site criado como exemplo</h2>
+    </footer>
+</body>
+</html>
